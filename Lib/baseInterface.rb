17370ccaa812ee7546b934ecd66fbd172cf2b04f
Xasin <logger89@web.de>
2018-03-01 13:13:03 +0100

    Removed WebSockets connection

--- Lib/baseInterface.rb
+++ Lib/baseInterface.rb
@@ -6,55 +6,6 @@ module Telemachus
 		def initialize(addr = "127.0.0.1:8085")
 			@data = Hash.new();
 			@address = "ws://#{addr}/datalink"
-
-			_configure_socket();
-			@guardThread = Thread.new do
-				loop do
-					sleep 0.5
-
-					if((Time.now - @lastReceived).to_f > 0.75)
-						_configure_socket();
-					end
-				end
-			end
-
-
-			nil
-		end
-
-		def _configure_socket()
-			base = self
-
-			@socket.close if @socket;
-
-			@socket = WebSocket::Client::Simple.connect(@address) do |socket|
-				socket.on :message do |msg|
-					base._handle_message(msg.data);
-				end
-
-				socket.on :error do |e|
-					puts "Socket had an error!! #{e}"
-					base._configure_socket
-				end
-
-				socket.on :open do
-					puts "Socket was opened!"
-					socket.send({:+ => ["p.paused"], rate: 500}.to_json)
-				end
-
-				socket.on :close do
-					puts "Socket was closed!"
-				end
-			end
-
-			@lastReceived = Time.now() + 1;
-			puts "Socket reconfigured!"
-		end
-
-		def _handle_message(data)
-			@lastReceived = Time.now();
-			@data = JSON.parse(data);
-		rescue
 		end
 
 		def _run(command, params = nil)
@@ -62,13 +13,6 @@ module Telemachus
 
 			argString = { run: [command].flatten }.to_json;
 			@socket.send(argString);
-		end
-
-		def +(topic)
-			@socket.send({:+ => [topic].flatten}.to_json)
-		end
-		def -(topic)
-			@socket.send({:- => [topic].flatten}.to_json)
 		end
 
 		def [](key)
